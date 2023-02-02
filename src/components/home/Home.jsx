8e3a0cbf65be5b598fd45e4ec0878591cdaca5f5
Steve Tambo <stevetambo32@gmail.com>
2023-02-02 22:39:29 +0300

    First Test Commit

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Hero from "./hero/Hero";
+
+const Home = () => {
+  return <Hero />;
+};
+
+//this is the home page component
+//home area
+
+export default Home;
