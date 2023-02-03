bd010fb16968b7e8d70b7b40de48870e39f444b6
Steve Tambo <stevetambo32@gmail.com>
2023-02-03 13:07:44 +0300

    Recents images added

--- src/components/home/recent/Recent.jsx
+++ src/components/home/recent/Recent.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import RecentCard from "./RecentCard";
+import "./recent.css";
+import Heading from "../../common/Heading";
 
 function Recent() {
-  return <>Recent</>;
+  return (
+    <>
+      <section className="recent padding">
+        <div className="container">
+          <Heading
+            title="Recent Properties Listed"
+            subtitle="Below are some of the most recent listings our agents have forwarded to us"
+          />
+          <RecentCard />
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Recent;
